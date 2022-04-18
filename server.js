32a5fb9bfb7762a1b5b920e0fda490542b1e3d98
gurel <guri240@gmail.com>
2022-04-18 23:56:11 +0300

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ivrit-app/index.html'));
+app.use(express.static(__dirname + '/dist/ivrit-app'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/ivrit-app/index.html'));
 });

501fdd469770a0f7b77963ebef1469206efc9981
gurel <guri240@gmail.com>
2022-04-18 23:31:57 +0300

    updated

---
+++ server.ts
@@ -0,0 +1,13 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/first-angular'));
+app.get('/*', function(req:any,res:any) {
+    res.sendFile(path.join(__dirname+'/dist/ivrit-app/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env['PORT'] || 8080);
